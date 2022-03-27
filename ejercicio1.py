af3b860ffeead23d731e32c3a27133a24dc05f79
alejandro1201 <102228998+alejandro1201@users.noreply.github.com>
2022-03-27 17:30:48 -0500

    Actividad en clase

---
+++ ejercicio1.py
@@ -0,0 +1,4 @@
+name = input("Inteoduzca su nombre, Por favor: ")
+surname = input("Introduzca su apellido, Por favor: ")
+
+print(f"{surname} , {name}")
---
+++ ejercicio4.py
@@ -0,0 +1,5 @@
+valor = 5
+valor = str(valor)
+resultado = int(valor) + int(valor *2) + int(valor *3)
+
+print(resultado)
